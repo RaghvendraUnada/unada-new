b637c110352616b3ef482620ca3138fa47b8935e
RaghvendraUnada <raghvendra@unada.io>
2023-03-06 16:13:10 +0530

    upload project

---
+++ src/views/HorizontalScrollingList.jsx
@@ -0,0 +1,22 @@
+import { Box, Stack } from "@mui/system";
+import Marquee from "react-fast-marquee";
+// import ICLWhite from "../../assets/images/clients/icl-white.svg";
+
+const HorizontalScrollingList = ({ children, fromRight }) => {
+  // console.log("fromRight",fromRight)
+  return (
+    <Marquee
+      gradient={false}
+      speed={10}
+      // pauseOnHover={true}
+      // pauseOnClick={true}
+      direction={fromRight ? "left" : "right"}
+      style={{
+        height: "400px",
+      }}
+    >
+      {children}
+    </Marquee>
+  );
+};
+export default HorizontalScrollingList;
---
+++ src/views/OurTeamMData.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import TeamImgOne from "../assets/Images/Home/OurTeam/ImgOne.png";
+import { Box, Typography } from "@mui/material";
+import Marquee from "react-fast-marquee";
+
+const OurTeamMData = (props) => {
+  console.log("props", props);
+  return (
+    <Marquee
+      gradient={false}
+      speed={15}
+      pauseOnHover={false}
+      // pauseOnClick={true}
+      direction={"left"}
+      style={{
+        width: "100%",
+        minWidth: "1200px",
+        height: "100%",
+        maxHeight: "500px",
+      }}
+    >
+      {props?.data?.map((client, idx) => {
+        console.log("client", client.TeamMemberImage[0]);
+        return (
+          <Box
+            sx={{
+              marginX: "12px",
+              borderRadius: "8px",
+              backgroundColor: "transparent",
+              transform: "rotate(-90deg)",
+              width: {
+                xs: "300px",
+                sm: "300px",
+                md: "350px",
+                lg: "400px",
+                xl: "400px",
+              },
+              height: {
+                xs: "auto",
+                sm: "auto",
+                md: "350px",
+                lg: "400px",
+                xl: "400px",
+              },
+              display: "grid",
+              justifyContent: "center",
+              alignItems: "end",
+            }}
+          >
+            <img
+              style={{
+                height: "auto",
+                maxHeight: "300px",
+                width: "auto",
+                marginLeft: "0px",
+              }}
+              src={client.TeamMemberImage}
+              alt="Team Member"
+            />
+            <Box>
+              <Typography sx={TextStyleOne}>{client.TeamMemberName}</Typography>
+              <Typography sx={TextStyleTwo}>{client.TeamMemberName}</Typography>
+            </Box>
+          </Box>
+        );
+      })}
+    </Marquee>
+  );
+};
+
+export default OurTeamMData;
+
+const TextStyleOne = {
+  fontFamily: "LGTrial",
+  fontStyle: "normal",
+  fontWeight: 500,
+  fontSize: "23px",
+  lineHeight: "36px",
+  letterSpacing: 0.1,
+  color: "#0F0F0F",
+};
+
+const TextStyleTwo = {
+  fontFamily: "LGTrial",
+  fontStyle: "normal",
+  fontWeight: 500,
+  fontSize: "15px",
+  lineHeight: "20px",
+  letterSpacing: 0.1,
+  color: "rgba(0, 0, 0, 0.76);",
+};
